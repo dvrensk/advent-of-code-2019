83355f71a138ce1195df0b12d3e0a79aae6ab7a6
David Vrensk <david@vrensk.com>
2019-12-04 22:01:30 +0100

    Day 3, wires on a grid, in Elixir - part 2

--- 3/lib/wire_grid.ex
+++ 3/lib/wire_grid.ex
@@ -4,6 +4,11 @@ defmodule WireGrid do
     manhattan_distance(str1, str2)
   end
 
+  def wire_distance_from_file(name) do
+    {str1, str2} = texts_from_file(name)
+    wire_distance(str1, str2)
+  end
+
   def texts_from_file(name) do
     File.stream!(name)
     |> Enum.to_list()
@@ -11,14 +16,33 @@ defmodule WireGrid do
   end
 
   def manhattan_distance(str1, str2) do
+    {wire1, wire2} = wires_from_texts(str1, str2)
+
+    common_nodes(wire1, wire2)
+    |> Enum.map(fn {x, y} -> abs(x) + abs(y) end)
+    |> Enum.min()
+  end
+
+  def wire_distance(str1, str2) do
+    {wire1, wire2} = wires_from_texts(str1, str2)
+
+    common_nodes(wire1, wire2)
+    |> Enum.map(fn node -> index(wire1, node) + index(wire2, node) end)
+    |> Enum.min()
+  end
+
+  def wires_from_texts(str1, str2) do
     [str1, str2]
     |> Enum.map(&wire_from_text/1)
+    |> List.to_tuple()
+  end
+
+  def common_nodes(wire1, wire2) do
+    [wire1, wire2]
     |> Enum.map(&MapSet.new/1)
     |> (fn [a, b] -> MapSet.intersection(a, b) end).()
     |> MapSet.to_list()
     |> List.delete({0, 0})
-    |> Enum.map(fn {x, y} -> abs(x) + abs(y) end)
-    |> Enum.min()
   end
 
   def wire_from_text(string) do
@@ -44,4 +68,6 @@ defmodule WireGrid do
   defp position("L", {x, y}), do: {x - 1, y}
   defp position("U", {x, y}), do: {x, y + 1}
   defp position("D", {x, y}), do: {x, y - 1}
+
+  defp index(list, e), do: Enum.find_index(list, fn e1 -> e == e1 end)
 end
--- 3/test/wire_grid_test.exs
+++ 3/test/wire_grid_test.exs
@@ -23,4 +23,22 @@ defmodule WireGridTest do
   test "quiz 1" do
     assert WireGrid.manhattan_from_file("input.txt") == 731
   end
+
+  test "example wire distances" do
+    assert WireGrid.wire_distance("R8,U5,L5,D3", "U7,R6,D4,L4") == 30
+
+    assert WireGrid.wire_distance(
+             "R75,D30,R83,U83,L12,D49,R71,U7,L72",
+             "U62,R66,U55,R34,D71,R55,D58,R83"
+           ) == 610
+
+    assert WireGrid.wire_distance(
+             "R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51",
+             "U98,R91,D20,R16,D67,R40,U7,R15,U6,R7"
+           ) == 410
+  end
+
+  test "quiz 2" do
+    assert WireGrid.wire_distance_from_file("input.txt") == 5672
+  end
 end
