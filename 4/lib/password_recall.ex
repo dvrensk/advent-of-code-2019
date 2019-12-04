2e465d953a70e24dea0583292ddbf28ffd71ca6c
David Vrensk <david@vrensk.com>
2019-12-04 11:41:18 +0100

    Day 4, password recall, second part

--- 4/lib/password_recall.ex
+++ 4/lib/password_recall.ex
@@ -2,8 +2,8 @@ defmodule PasswordRecall do
   def in_range(range) do
     range
     |> decorate()
-    |> only_increasing()
-    |> at_least_one_double()
+    |> Enum.reject(fn {_, digits} -> decreasing_sequence?(digits) end)
+    |> Enum.filter(fn {_, digits} -> at_least_one_true_double?(digits) end)
     |> remove_decoration()
   end
 
@@ -11,23 +11,20 @@ defmodule PasswordRecall do
     Enum.map(numbers, fn a -> {a, Integer.digits(a)} end)
   end
 
-  defp only_increasing(numbers) do
-    numbers
-    |> Enum.reject(fn {_, digits} -> decreasing_sequence?(digits) end)
-  end
-
   defp decreasing_sequence?([_]), do: false
   defp decreasing_sequence?([a, b | _]) when a > b, do: true
   defp decreasing_sequence?([_ | tail]), do: decreasing_sequence?(tail)
 
-  defp at_least_one_double(numbers) do
-    numbers
-    |> Enum.filter(fn {_, digits} -> at_least_one_double?(digits) end)
+  defp at_least_one_true_double?([]), do: false
+
+  defp at_least_one_true_double?([a, a, a | tail]) do
+    tail
+    |> Enum.drop_while(fn x -> x == a end)
+    |> at_least_one_true_double?()
   end
 
-  defp at_least_one_double?([_]), do: false
-  defp at_least_one_double?([a, a | _]), do: true
-  defp at_least_one_double?([_ | tail]), do: at_least_one_double?(tail)
+  defp at_least_one_true_double?([a, a | _]), do: true
+  defp at_least_one_true_double?([_ | tail]), do: at_least_one_true_double?(tail)
 
   defp remove_decoration(numbers) do
     Enum.map(numbers, fn {a, _digits} -> a end)
--- 4/test/password_recall_test.exs
+++ 4/test/password_recall_test.exs
@@ -9,11 +9,11 @@ defmodule PasswordRecallTest do
     assert PasswordRecall.in_range(223_450..223_450) == []
   end
 
-  test "at least one double" do
-    assert PasswordRecall.in_range(123_455..123_456) == [123_455]
+  test "at least one true double" do
+    assert PasswordRecall.in_range(123_339..123_344) == [123_344]
   end
 
-  test "finds the candidates for the quiz" do
-    assert Enum.count(PasswordRecall.in_range(145_852..616_942)) == 1767
+  test "finds the candidates for the second quiz" do
+    assert Enum.count(PasswordRecall.in_range(145_852..616_942)) == 1192
   end
 end
