2fd556468d7ead37cf7e7bb47778e24aaf98b787
David Vrensk <david@vrensk.com>
2019-12-06 00:26:19 +0100

    starting over from scratch would have been easier, I think

--- 5/lib/intcode.ex
+++ 5/lib/intcode.ex
@@ -1,170 +1,56 @@
 defmodule Intcode do
   import Enum, only: [at: 2]
 
-  def run(memory, ip \\ 0) do
-    case calc(memory, ip) do
-      {:update, pos, val} ->
-        List.replace_at(memory, pos, val)
-        |> run(ip + 4)
+  def run(memory), do: run(memory, %{})
 
-      {:halt} ->
-        memory
-    end
+  def run(memory, context) do
+    context =
+      context
+      |> Map.put_new(:ip, 0)
+      |> Map.put_new(:input, [])
+      |> Map.put_new(:output, [])
+
+    run(at(memory, context.ip), memory, context)
   end
 
   @add 1
   @mul 2
+  @read 3
+  @write 4
   @hlt 99
 
-  defp calc(memory, ip) do
-    case at(memory, ip) do
-      @add ->
-        {:update, at(memory, ip + 3), value(memory, ip + 1) + value(memory, ip + 2)}
-
-      @mul ->
-        {:update, at(memory, ip + 3), value(memory, ip + 1) * value(memory, ip + 2)}
-
-      @hlt ->
-        {:halt}
-    end
+  def run(@add, memory, context = %{ip: ip}) do
+    memory
+    |> update(ip + 3, atat(memory, ip + 1) + atat(memory, ip + 2))
+    |> run(%{context | ip: ip + 4})
   end
 
-  defp value(memory, pos) do
+  def run(@mul, memory, context = %{ip: ip}) do
+    memory
+    |> update(ip + 3, atat(memory, ip + 1) * atat(memory, ip + 2))
+    |> run(%{context | ip: ip + 4})
+  end
+
+  def run(@read, memory, context = %{ip: ip, input: [head | tail]}) do
+    memory
+    |> update(ip + 1, head)
+    |> run(%{context | ip: ip + 2, input: tail})
+  end
+
+  def run(@write, memory, context = %{ip: ip, output: output}) do
+    run(memory, %{context | ip: ip + 2, output: [atat(memory, ip+1) | output]})
+  end
+
+  def run(@hlt, memory, context = %{output: output}) do
+    %{context | output: Enum.reverse(output)}
+    |> Map.put_new(:memory, memory)
+  end
+
+  defp update(memory, position, value) do
+    List.replace_at(memory, at(memory, position), value)
+  end
+
+  defp atat(memory, pos) do
     at(memory, at(memory, pos))
-  end
-
-  def find(output, nounverb \\ 0) when nounverb < 10000 do
-    noun = div(nounverb, 100)
-    verb = Integer.mod(nounverb, 100)
-
-    case hd(run(well_known_program(noun, verb))) do
-      ^output ->
-        nounverb
-
-      _ ->
-        find(output, nounverb + 1)
-    end
-  end
-
-  def well_known_program(noun, verb) do
-    [
-      1,
-      noun,
-      verb,
-      3,
-      1,
-      1,
-      2,
-      3,
-      1,
-      3,
-      4,
-      3,
-      1,
-      5,
-      0,
-      3,
-      2,
-      1,
-      9,
-      19,
-      1,
-      19,
-      5,
-      23,
-      2,
-      23,
-      13,
-      27,
-      1,
-      10,
-      27,
-      31,
-      2,
-      31,
-      6,
-      35,
-      1,
-      5,
-      35,
-      39,
-      1,
-      39,
-      10,
-      43,
-      2,
-      9,
-      43,
-      47,
-      1,
-      47,
-      5,
-      51,
-      2,
-      51,
-      9,
-      55,
-      1,
-      13,
-      55,
-      59,
-      1,
-      13,
-      59,
-      63,
-      1,
-      6,
-      63,
-      67,
-      2,
-      13,
-      67,
-      71,
-      1,
-      10,
-      71,
-      75,
-      2,
-      13,
-      75,
-      79,
-      1,
-      5,
-      79,
-      83,
-      2,
-      83,
-      9,
-      87,
-      2,
-      87,
-      13,
-      91,
-      1,
-      91,
-      5,
-      95,
-      2,
-      9,
-      95,
-      99,
-      1,
-      99,
-      5,
-      103,
-      1,
-      2,
-      103,
-      107,
-      1,
-      10,
-      107,
-      0,
-      99,
-      2,
-      14,
-      0,
-      0
-    ]
   end
 end
--- 5/test/intcode_test.exs
+++ 5/test/intcode_test.exs
@@ -2,27 +2,17 @@ defmodule IntcodeTest do
   use ExUnit.Case
 
   test "basic examples" do
-    assert Intcode.run([1, 0, 0, 0, 99]) == [2, 0, 0, 0, 99]
-    assert Intcode.run([2, 3, 0, 3, 99]) == [2, 3, 0, 6, 99]
-    assert Intcode.run([2, 4, 4, 5, 99, 0]) == [2, 4, 4, 5, 99, 9801]
-    assert Intcode.run([1, 1, 1, 4, 99, 5, 6, 0, 99]) == [30, 1, 1, 4, 2, 5, 6, 0, 99]
+    assert Intcode.run([1, 0, 0, 0, 99]).memory == [2, 0, 0, 0, 99]
+    assert Intcode.run([2, 3, 0, 3, 99]).memory == [2, 3, 0, 6, 99]
+    assert Intcode.run([2, 4, 4, 5, 99, 0]).memory == [2, 4, 4, 5, 99, 9801]
+    assert Intcode.run([1, 1, 1, 4, 99, 5, 6, 0, 99]).memory == [30, 1, 1, 4, 2, 5, 6, 0, 99]
   end
 
-  # test "run with 1202" do
-  #   input =
-  #     File.read!("input.txt")
-  #     |> String.split(",")
-  #     |> Enum.map(&String.to_integer/1)
-  #     |> List.replace_at(1, 12)
-  #     |> List.replace_at(2, 2)
-  #   assert hd(Intcode.run(input)) == 3895705
-  # end
-
-  test "run with 1202" do
-    assert hd(Intcode.run(Intcode.well_known_program(12, 2))) == 3_895_705
+  test "reading input" do
+    assert Intcode.run([3, 0, 3, 1, 99], %{input: [33, 44]}).memory == [33, 44, 3, 1, 99]
   end
 
-  test "find noun+verb for 19690720" do
-    assert Intcode.find(19_690_720) == 6417
+  test "writing output" do
+    assert Intcode.run([4, 0, 4, 4, 99]).output == [4,99]
   end
 end
