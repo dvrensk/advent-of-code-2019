5560de842211cd37fcc3907c7464e7f822bb777a
David Vrensk <david@vrensk.com>
2019-12-10 14:50:14 +0100

    Day 8, Space Image Format in Elixir. Part 2.

--- 8/lib/space_image_format.ex
+++ 8/lib/space_image_format.ex
@@ -8,6 +8,32 @@ defmodule SpaceImageFormat do
     |> Enum.to_list()
   end
 
+  def merge_down([top | rest]), do: merge_down(top, rest)
+
+  def merge_down(merged, []), do: merged
+
+  def merge_down(a, [b | rest]) do
+    width = length(hd(a))
+
+    merge(List.flatten(a), List.flatten(b), [])
+    |> Stream.chunk_every(width)
+    |> Enum.to_list()
+    |> merge_down(rest)
+  end
+
+  def merge([], [], ack), do: Enum.reverse(ack)
+  def merge([2 | as], [b | bs], ack), do: merge(as, bs, [b | ack])
+  def merge([a | as], [_ | bs], ack), do: merge(as, bs, [a | ack])
+
+  def render(layer) do
+    layer
+    |> Enum.map(fn row -> row |> Enum.map(&Integer.to_string/1) |> Enum.join() end)
+    |> Enum.join("\n")
+    |> String.replace("1", "8")
+    |> String.replace("0", "·")
+    |> (fn s -> s <> "\n" end).()
+  end
+
   def count_digit(layer, digit) do
     List.flatten(layer)
     |> Enum.count(fn e -> e == digit end)
--- 8/test/space_image_format_test.exs
+++ 8/test/space_image_format_test.exs
@@ -21,4 +21,24 @@ defmodule SpaceImageFormatTest do
 
     assert SpaceImageFormat.count_digit(layer, 1) * SpaceImageFormat.count_digit(layer, 2) == 2684
   end
+
+  test "it flattens layers with transparency" do
+    layers = SpaceImageFormat.parse("0222112222120000", 2, 2)
+    assert SpaceImageFormat.merge_down(layers) == [[0, 1], [1, 0]]
+  end
+
+  test "it solves part 2" do
+    layers = SpaceImageFormat.parse(File.read!("input.txt"), 25, 6)
+
+    ygryz = """
+    8···8·88··888··8···88888·
+    8···88··8·8··8·8···8···8·
+    ·8·8·8····8··8··8·8···8··
+    ··8··8·88·888····8···8···
+    ··8··8··8·8·8····8··8····
+    ··8···888·8··8···8··8888·
+    """
+
+    assert SpaceImageFormat.render(SpaceImageFormat.merge_down(layers)) == ygryz
+  end
 end
